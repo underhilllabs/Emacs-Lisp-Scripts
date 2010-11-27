f49e8b72733b90fc06f2af73f7f2a205484090c6
Bart Lantz <bartlantz@gmail.com>
2010-11-27 14:32:42 -0700

    updated some exercises

--- interview_questions.el
+++ interview_questions.el
@@ -29,8 +29,34 @@
       (setq rstr (cons (pop mystr) rstr)))
     (apply 'string rstr)))
 (myreverse "one ring")
- 
-		     
+
+(defun mult_grid (n)
+  "create an n-by-n multiplication grid"
+  (interactive "nHow big?")
+  (with-temp-buffer 
+    (let ((i 1))
+      (while (< i (1+ n))
+	 (loop for var from 1 to n
+	       do (princ (format "%4d" (* i var))))
+	 (terpri)
+	(setq i (1+ i))
+	)
+      ))
+(terpri))
+(mult_grid 12)
+
+(defun fib (n)
+  "fibonacci series up to n"
+  (interactive "nEnter Level: ")
+  (if (< n 2)
+      n
+    (+ (fib (- n 1)) (fib (- n 2)))))
+(fib 30)
+
+(defun multi_grid ()
+  (mapcar '* '(1 2 3 4 5 6) '(1 2 3 4 5 6)))
+(multi_grid)
+
 ;; (defun sum-ints-file (fname)
 ;;   "Sum integers in a file, one integer per line"
 ;;   (interactive "sEnter file name: ")
